c0273fdefd65513170e5f25359d0b8258bee5855
Nikita Radetskiy <idisambiguation@gmail.com>
2017-03-06 23:41:36 +0400

    setup finished. at first look

--- src/app/index.ts
+++
@@ -1,8 +0,0 @@
-function genericArray<T>(element:T): Array<T> {
-    let list: Array<T> = [element];
-    return list;
-}
-
-let test: Array<number> = genericArray<number>(1);
-
-export { genericArray, test };
